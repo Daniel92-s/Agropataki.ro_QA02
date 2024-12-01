e3c362c1a65b20c68ebf3621c453ee85efcdd9d0
Daniel <dany_dany_92_2007@yahoo.com>
2024-12-01 17:50:23 +0100

    Regresion / test data / tests

---
+++ src/test/java/data/LoginDP.java
@@ -0,0 +1,15 @@
+package data;
+
+import org.testng.annotations.DataProvider;
+
+public class LoginDP {
+    @DataProvider
+    public Object[][] loginDataProvider() {
+        return new Object[][]{
+//          username, password, browser
+                {"zebra", "zebrapassword", "edge"},
+                {"dingo", "dingopassword", "chrome"},
+                {"camel", "camelpassword", "edge"},
+        };
+    }
+}
